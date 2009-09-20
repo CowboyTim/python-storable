57ec34b51c9736a797bdfb6fe2e2a18218c6f4d1
CowboyTim <aardbeiplantje@gmail.com>
2009-09-20 16:27:31 +0200

    - cleaned up the print()'s a bit - added SX_OVERLOAD - fixed bug with SX_SV_UNDEF: wrong hex magic

--- storable.py
+++ storable.py
@@ -28,6 +28,127 @@
 
 from struct import unpack
 
+
+def SX_OBJECT(fh, cache):
+    i = unpack('i', fh.read(4))[0]
+    return cache['objects'][i-1]
+
+def SX_LSCALAR(fh, cache):
+    size = unpack('i', fh.read(4))[0]
+    return fh.read(size)
+
+def SX_LUTF8STR(fh, cache):
+    return SX_LSCALAR(fh, cache)
+
+def SX_ARRAY(fh, cache):
+    size = unpack('i', fh.read(4))[0]
+    return [process_item(fh, cache) for i in range(0,size)]
+
+def SX_HASH(fh, cache):
+    size = unpack('i', fh.read(4))[0]
+    data = {}
+    for i in range(0,size):
+        value = process_item(fh, cache)
+        size  = unpack('i', fh.read(4))[0]
+        key   = fh.read(size)
+        data[key] = value
+
+    return data
+
+def SX_REF(fh, cache):
+    # in fact: ignored, python doesn't have/need them, although a GOTO
+    # would consume less memory/cpu by not executing another method ;-)
+    return process_item(fh, cache)
+
+def SX_UNDEF(fh, cache):
+    return None
+
+def SX_DOUBLE(fh, cache):
+    return unpack('d', fh.read(8))[0]
+
+def SX_BYTE(fh, cache):
+    return unpack('B', fh.read(1))[0] - 128
+
+def SX_SCALAR(fh, cache):
+    size = unpack('B', fh.read(1))[0]
+    return fh.read(size)
+
+def SX_UTF8STR(fh, cache):
+    return SX_SCALAR(fh, cache)
+
+def SX_TIED_ARRAY(fh, cache):
+    # in fact: ignored, python doesn't have/need them, although a GOTO
+    # would consume less memory/cpu by not executing another method ;-)
+    return process_item(fh, cache)
+
+def SX_TIED_HASH(fh, cache):
+    return SX_TIED_ARRAY(fh, cache)
+
+def SX_TIED_SCALAR(fh, cache):
+    return SX_TIED_ARRAY(fh, cache)
+
+def SX_SV_UNDEF(fh, cache):
+    return None
+
+def SX_BLESS(fh, cache):
+    size = unpack('B', fh.read(1))[0]
+    package_name = fh.read(size)
+    cache['classes'].append(package_name)
+    return process_item(fh, cache)
+
+def SX_IX_BLESS(fh, cache):
+    indx = unpack('B', fh.read(1))[0]
+    package_name = cache['classes'][indx]
+    return process_item(fh, cache)
+
+def SX_OVERLOAD(fh, cache):
+    return process_item(fh, cache)
+
+def SX_TIED_KEY(fh, cache):
+    data = process_item(fh, cache)
+    key  = process_item(fh, cache)
+    return data
+    
+def SX_TIED_IDX(fh, cache):
+    data = process_item(fh, cache)
+    indx_in_array = unpack('i', fh.read(4))[0]
+    return data
+
+# *AFTER* all the subroutines
+engine = {
+    '\x00': SX_OBJECT,      # ( 0): Already stored object
+    '\x01': SX_LSCALAR,     # ( 1): Scalar (large binary) follows (length, data)
+    '\x02': SX_ARRAY,       # ( 2): Array forthcoming (size, item list)
+    '\x03': SX_HASH,        # ( 3): Hash forthcoming (size, key/value pair list)
+    '\x04': SX_REF,         # ( 4): Reference to object forthcoming
+    '\x05': SX_UNDEF,       # ( 5): Undefined scalar
+    '\x07': SX_DOUBLE,      # ( 7): Double forthcoming
+    '\x08': SX_BYTE,        # ( 8): (signed) byte forthcoming
+    '\x0a': SX_SCALAR,      # (10): Scalar (binary, small) follows (length, data)
+    '\x0b': SX_TIED_ARRAY,  # (11): Tied array forthcoming
+    '\x0c': SX_TIED_HASH,   # (12): Tied hash forthcoming
+    '\x0d': SX_TIED_SCALAR, # (13): scalar hash forthcoming
+    '\x0e': SX_SV_UNDEF,    # (14): Perl's immortal PL_sv_undef
+    '\x11': SX_BLESS,       # (17): Object is blessed
+    '\x12': SX_IX_BLESS,    # (18): Object is blessed, classname given by index
+    '\x14': SX_OVERLOAD,    # (20): Overloaded reference
+    '\x15': SX_TIED_KEY,    # (21): Tied magic key forthcoming
+    '\x16': SX_TIED_IDX,    # (22): Tied magic index forthcoming
+    '\x17': SX_UTF8STR,     # (23): UTF-8 string forthcoming (small)
+    '\x18': SX_LUTF8STR,    # (24): UTF-8 string forthcoming (large)
+}
+
+def process_item(fh, cache):
+    data = None
+    magic_type = fh.read(1)
+    if magic_type in engine:
+        print(engine[magic_type])
+        data = engine[magic_type](fh, cache)
+
+    cache['objects'].append(data)
+    print(cache)
+    return data
+
 def thaw(fh):
     magic = fh.read(2)
     if magic == '\x05\x07':
@@ -39,87 +160,4 @@ def thaw(fh):
 
     cache = { 'objects' : [], 'classes' : [] } 
     return process_item(fh, cache)
-
-def process_item(fh, cache):
-    data = None
-    magic_type = fh.read(1)
-    if   magic_type == '\x00':  # SX_OBJECT      ( 0): Already stored object
-        i = unpack('i', fh.read(4))[0]
-        print("stored object, nr:"+str(i))
-        data = cache['objects'][i-1]
-    elif magic_type == '\x01' \
-      or magic_type == '\x18':
-                                # SX_LSCALAR     ( 1): Scalar (large binary) follows (length, data)
-                                # SX_LUTF8STR    (24): UTF-8 string forthcoming (large)
-        size = unpack('i', fh.read(4))[0]
-        print("big scalar, size:"+str(size))
-        data = fh.read(size)
-    elif magic_type == '\x02':  # SX_ARRAY       ( 2): Array forthcoming (size, item list)
-        size = unpack('i', fh.read(4))[0]
-        print("array, size:"+str(size))
-        data = [process_item(fh, cache) for i in range(0,size)]
-    elif magic_type == '\x03':  # SX_HASH        ( 3): Hash forthcoming (size, key/value pair list)
-        size = unpack('i', fh.read(4))[0]
-        print("array, size:"+str(size))
-        data = {}
-        for i in range(0,size):
-            value = process_item(fh, cache)
-            size  = unpack('i', fh.read(4))[0]
-            key   = fh.read(size)
-            data[key] = value
-    elif magic_type == '\x04':  # SX_REF         ( 4): Reference to object forthcoming
-        print("ref to next")
-        # in fact: ignored, python doesn't have/need them, although a GOTO
-        # would consume less memory/cpu by not executing another method ;-)
-        data = process_item(fh, cache)
-    elif magic_type == '\x05':  # SX_UNDEF       ( 5): Undefined scalar
-        print("scalar undef")
-        data = None
-    elif magic_type == '\x07':  # SX_DOUBLE      ( 7): Double forthcoming
-        print("double")
-        data = unpack('d', fh.read(8))[0]
-    elif magic_type == '\x08':  # SX_BYTE        ( 8): (signed) byte forthcoming
-        print("scalar byte")
-        data = unpack('B', fh.read(1))[0] - 128
-    elif magic_type == '\x0a' \
-      or magic_type == '\x17':
-                                # SX_SCALAR      (10): Scalar (binary, small) follows (length, data)
-                                # SX_UTF8STR     (23): UTF-8 string forthcoming (small)
-        size = unpack('B', fh.read(1))[0]
-        print("small scalar, size:"+str(size))
-        data = fh.read(size)
-    elif magic_type == '\x0b' \
-      or magic_type == '\x0c' \
-      or magic_type == '\x0d':
-                                # SX_TIED_ARRAY  (11): Tied array forthcoming
-                                # SX_TIED_HASH   (12): Tied hash forthcoming
-                                # SX_TIED_SCALAR (13): scalar hash forthcoming
-        print("tied")
-        # in fact: ignored, python doesn't have/need them, although a GOTO
-        # would consume less memory/cpu by not executing another method ;-)
-        data = process_item(fh, cache)
-    elif magic_type == '\x14':  # SX_SV_UNDEF    (14): Perl's immortal PL_sv_undef
-        print("scalar undef")
-    elif magic_type == '\x11':  # SX_BLESS       (17): Object is blessed
-        size = unpack('B', fh.read(1))[0]
-        package_name = fh.read(size)
-        cache['classes'].append(package_name)
-        data = process_item(fh, cache)
-    elif magic_type == '\x12':  # SX_IX_BLESS    (18): Object is blessed, classname given by index
-        indx = unpack('B', fh.read(1))[0]
-        package_name = cache['classes'][indx]
-        data = process_item(fh, cache)
-    elif magic_type == '\x15':  # SX_TIED_KEY    (21): Tied magic key forthcoming
-        data = process_item(fh, cache)
-        key  = process_item(fh, cache)
-    elif magic_type == '\x16':  # SX_TIED_IDX    (22): Tied magic index forthcoming
-        data = process_item(fh, cache)
-        indx_in_array = unpack('i', fh.read(4))[0]
-        
-    else:
-        return None
-
-    cache['objects'].append(data)
-    print(cache)
-    return data
 
