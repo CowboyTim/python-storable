34a8bb20e0c7938e63abeae96264614cfe081b29
CowboyTim <aardbeiplantje@gmail.com>
2009-09-29 22:27:02 +0200

    - adds pickle'd resource files

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python
 
+import pickle
 import glob
 
 from storable import thaw
 
-for f in glob.glob('/python-storable/resources/*nfreeze.storable'):
-    print(f)
+for infile in glob.glob('resources/*nfreeze.storable'):
     # simple test: read data
-    fh = open(f, 'rb')
-    data = fh.read()
-    fh.close()
+    print('reading from '+infile)
+    infh = open(infile, 'rb')
+    data = infh.read()
+    infh.close()
 
     # thaw() it
     data = thaw(data)
 
     # dump it
+    outfile = infile + '.pickle'
+    print('writing output to '+outfile)
+    outfh = open(outfile,'wb')
+    pickle.dump(data, outfh)
+    outfh.close()
+    
     print(data)
