3ea846e82fff77adaf9a7035706027a1d637267f
CowboyTim <aardbeiplantje@gmail.com>
2009-09-30 20:35:15 +0200

    - results updated of the selftests for the circular references

--- resources/results/040_complex01.py
+++ resources/results/040_complex01.py
@@ -1,1 +1,1 @@
-[None, [None, None, None, None, None, 'x'], None, None, None]
+[None, [None, None, None, None, None, 'x'], [None, None, None, None, None, 'x'], [None, None, None, None, None, 'x'], [None, None, None, None, None, 'x']]
