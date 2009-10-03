3fac37a845d985de5046dbfad05191b4975e22d1
CowboyTim <aardbeiplantje@gmail.com>
2009-10-03 21:20:49 +0200

    - added a distutils setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='storable',
+      version='1.0',
+      description='Python Perl Storable module',
+      author='Tim Aerts',
+      author_email='aardbeiplantje@gmail.com',
+      url='http://github.com/CowoboyTim/python-storable',
+      py_modules=['storable'],
+     )
+
