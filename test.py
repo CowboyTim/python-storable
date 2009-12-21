c54f7987f137d3b1f5e79d59866f25775f946fc0
CowboyTim <aardbeiplantje@gmail.com>
2009-12-21 16:19:18 +0100

    - selftest cleanup

--- test.py
+++ test.py
@@ -8,6 +8,27 @@ from os.path import basename
 
 import storable
 
+# search for the special tests where freeze <-> nfreeze (same for
+# store/nstore). Those tests really do have a seperate result file. In such a
+# case, we take the other .store.py file instead of the plain .py file as a
+# result to compare with
+
+special_tests = {}
+for result in sorted(glob.glob('t/results/*.freeze.py')):
+    result = basename(result)
+    result = search(r'(.*)\.freeze\.py', result).group(1)
+    special_tests[result] = 1
+
+def determine_outfile(infile):
+    outfile = basename(infile)
+    group = match(r"^(.*)_\d+\.\d+_.*_(freeze|nfreeze|store|nstore)\.storable$", outfile)
+    testcase = group.group(1)
+    freeze   = group.group(2)
+    if freeze == 'freeze' and testcase in special_tests:
+        return 't/results/' + testcase + '.freeze.py'
+    else:
+        return 't/results/' + testcase + '.py'
+        
 class TestStorable(unittest.TestCase):
 
     def test_sun4_solaris_nfreeze(self):
@@ -35,34 +56,8 @@ class TestStorable(unittest.TestCase):
             self.do_test(infile, deserializer=lambda f:str(storable.retrieve(f)))
 
     def test_freeze(self):
-        special_tests = {}
-        for result in sorted(glob.glob('t/results/*.freeze.py')):
-            result = basename(result)
-            result = search(r'(.*)\.freeze\.py', result).group(1)
-            special_tests[result] = 1
-
         for infile in sorted(glob.glob('t/resources/*/*/*_freeze.storable')):
-            outfile = basename(infile)
-            group = match(r"^(.*)_\d+\.\d+_.*_(freeze|nfreeze)\.storable$", outfile)
-            testcase = group.group(1)
-            if testcase not in special_tests:
-                outfile  = 't/results/' + testcase + '.py'
-                self.do_test(infile, outfile)
-
-    def test_freeze_special_cases(self):
-        special_tests = {}
-        for result in sorted(glob.glob('t/results/*.freeze.py')):
-            result = basename(result)
-            result = search(r'(.*)\.freeze\.py', result).group(1)
-            special_tests[result] = 1
-
-        for infile in sorted(glob.glob('t/resources/*/*/*_freeze.storable')):
-            outfile = basename(infile)
-            group = match(r"^(.*)_\d+\.\d+_.*_(freeze|nfreeze)\.storable$", outfile)
-            testcase = group.group(1)
-            if testcase in special_tests:
-                outfile  = 't/results/' + testcase + '.freeze.py'
-                self.do_test(infile, outfile)
+            self.do_test(infile)
 
     def mythaw(infile):
 
@@ -79,18 +74,14 @@ class TestStorable(unittest.TestCase):
 
         return data
 
-    def do_test(self, infile, outfile=None, deserializer=mythaw):
+    def do_test(self, infile, deserializer=mythaw):
 
         data = deserializer(infile)
 
         result_we_need = None
 
         # read the to-be-result in
-        if not outfile:
-            outfile = basename(infile)
-            group = match(r"^(.*)_\d+\.\d+_.*_(freeze|nfreeze|store|nstore)\.storable$", outfile)
-            testcase = group.group(1)
-            outfile  = 't/results/' + testcase + '.py'
+        outfile  = determine_outfile(infile)
         try:
             outfh = open(outfile,'rb')
             result_we_need = outfh.read()
