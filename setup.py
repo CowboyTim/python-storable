d0a7cc00cbbc6f8f00413126612f4122e966995b
CowboyTim <aardbeiplantje@gmail.com>
2014-01-17 22:16:06 +0100

    setup.py: added LICENSE & README

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(name='storable',
       author='Tim Aerts',
       author_email='aardbeiplantje@gmail.com',
       url='http://github.com/CowoboyTim/python-storable',
+      license='LICENSE.txt',
       py_modules=['storable'],
+      long_description=open('README.txt').read(),
      )
 
