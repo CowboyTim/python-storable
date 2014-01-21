edc1fdf6ca83ce483b8af5fcb8530852f7151f43
CowboyTim <aardbeiplantje@gmail.com>
2014-01-21 22:44:42 +0100

    setup.py: more specificness

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='storable',
-      version='1.0',
-      description='Python Perl Storable module',
-      author='Tim Aerts',
-      author_email='aardbeiplantje@gmail.com',
-      url='http://github.com/CowoboyTim/python-storable',
-      license='LICENSE.txt',
-      py_modules=['storable'],
-      long_description=open('README.txt').read(),
-     )
-
+setup(
+    name             = 'storable',
+    version          = '1.0',
+    description      = 'Python Perl Storable module',
+    author           = 'Tim Aerts',
+    author_email     = 'aardbeiplantje@gmail.com',
+    url              = 'http://github.com/CowoboyTim/python-storable',
+    license          = 'LICENSE.txt',
+    py_modules       = ['storable'],
+    long_description = open('README.txt').read(),
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+    ]
+)
