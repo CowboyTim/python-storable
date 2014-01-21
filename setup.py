3d11edf75ae9620a752f8804d4ea8d0e6bd539ed
CowboyTim <aardbeiplantje@gmail.com>
2014-01-21 23:14:25 +0100

    setup.py: version set to 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name             = 'storable',
-    version          = '1.0',
+    version          = '0.1.0',
     description      = 'Python Perl Storable module',
     author           = 'Tim Aerts',
     author_email     = 'aardbeiplantje@gmail.com',
