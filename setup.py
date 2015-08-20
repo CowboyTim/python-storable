7d8ef55cc1bc613777eeb2cdf9721b21494aa12e
Michael Hart <michael.hart@zpg.co.uk>
2015-08-20 12:28:15 +0100

    Multiple changes
    - Make UTF-8 strings result in Python unicode objects
    - Add yes, no and vstring support including test output to trigger these
      in test.pl
    - Try to rationalise use of storable-internal sizes (int/long/nv/ptr)
      (NEEDS WORK)
    - Bring closer to PEP8 compliance
    - use setuptools to allow use of python setup.py develop to ease testing

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name             = 'storable',
-    version          = '0.1.0',
-    description      = 'Python Perl Storable module',
-    author           = 'CowboyTim',
-    author_email     = 'aardbeiplantje@gmail.com',
-    url              = 'http://github.com/CowboyTim/python-storable',
-    license          = 'LICENSE.txt',
-    py_modules       = ['storable'],
-    long_description = open('README.txt').read(),
-    classifiers      = [
+    name='storable',
+    version='0.1.0',
+    description='Python Perl Storable module',
+    author='CowboyTim',
+    author_email='aardbeiplantje@gmail.com',
+    url='http://github.com/CowboyTim/python-storable',
+    license='LICENSE.txt',
+    py_modules=['storable'],
+    long_description=open('README.txt').read(),
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Perl',
--- storable.py
+++ storable.py
@@ -26,179 +26,185 @@
 # Tim Aerts <aardbeiplantje@gmail.com>
 #
 
-from struct import unpack
-import cStringIO
+from struct import (calcsize, unpack)
+import cStringIO as StringIO
+
 
 def _read_size(fh, cache):
-    return unpack(cache['size_unpack_fmt'], fh.read(4))[0]
+    fmt = cache['size_unpack_fmt']
+    return unpack(fmt, fh.read(calcsize(fmt)))[0]
+
 
 def SX_OBJECT(fh, cache):
-    # idx's are always big-endian dumped by storable's freeze/nfreeze I think
-    i = unpack('>I', fh.read(4))[0]
+    # From Storable.xs store function:
+    # * The tag is always written in network order.
+    i = SX_NETINT(fh, cache)
     cache['has_sx_object'] = True
     return (0, i)
+
 
 def SX_LSCALAR(fh, cache):
     return fh.read(_read_size(fh, cache))
 
+
 def SX_LUTF8STR(fh, cache):
-    return SX_LSCALAR(fh, cache)
+    return SX_LSCALAR(fh, cache).decode('utf-8')
+
 
 def SX_ARRAY(fh, cache):
-    data = []
-    for i in range(0,_read_size(fh, cache)):
-        data.append(process_item(fh, cache))
+    return [process_item(fh, cache) for i in xrange(_read_size(fh, cache))]
 
-    return data
 
 def SX_HASH(fh, cache):
     data = {}
-    for i in range(0,_read_size(fh, cache)):
+    for i in xrange(_read_size(fh, cache)):
         value = process_item(fh, cache)
-        key   = fh.read(_read_size(fh, cache))
+        key = fh.read(_read_size(fh, cache))
         data[key] = value
-
     return data
+
 
 def SX_REF(fh, cache):
     return process_item(fh, cache)
 
+
 def SX_UNDEF(fh, cache):
     return None
 
+
 def SX_INTEGER(fh, cache):
-    return unpack(cache['int_unpack_fmt'], fh.read(8))[0]
+    fmt = cache['int_unpack_fmt']
+    return unpack(fmt, fh.read(calcsize(fmt)))[0]
+
 
 def SX_DOUBLE(fh, cache):
-    return unpack(cache['double_unpack_fmt'], fh.read(8))[0]
+    fmt = cache['double_unpack_fmt']
+    return unpack(fmt, fh.read(calcsize(fmt)))[0]
+
 
 def SX_BYTE(fh, cache):
-    return unpack('B', fh.read(1))[0] - 128
+    return _read_unsigned_byte(fh) - 128
+
 
 def SX_NETINT(fh, cache):
-    return unpack('>I', fh.read(4))[0]
+    fmt = '!I'
+    return unpack(fmt, fh.read(calcsize(fmt)))[0]
+
 
 def SX_SCALAR(fh, cache):
-    size = unpack('B', fh.read(1))[0]
+    size = _read_unsigned_byte(fh)
     return fh.read(size)
 
+
 def SX_UTF8STR(fh, cache):
-    return SX_SCALAR(fh, cache)
+    return SX_SCALAR(fh, cache).decode('utf-8')
+
 
 def SX_TIED_ARRAY(fh, cache):
     return process_item(fh, cache)
 
+
 def SX_TIED_HASH(fh, cache):
     return SX_TIED_ARRAY(fh, cache)
+
 
 def SX_TIED_SCALAR(fh, cache):
     return SX_TIED_ARRAY(fh, cache)
 
+
 def SX_SV_UNDEF(fh, cache):
     return None
 
+
+def SX_SV_YES(fh, cache):
+    return True
+
+
+def SX_SV_NO(fh, cache):
+    return False
+
+
 def SX_BLESS(fh, cache):
-    size = unpack('B', fh.read(1))[0]
+    size = _read_unsigned_byte(fh)
     package_name = fh.read(size)
     cache['classes'].append(package_name)
     return process_item(fh, cache)
 
+
 def SX_IX_BLESS(fh, cache):
-    indx = unpack('B', fh.read(1))[0]
+    indx = _read_unsigned_byte(fh)
     package_name = cache['classes'][indx]
     return process_item(fh, cache)
+
 
 def SX_OVERLOAD(fh, cache):
     return process_item(fh, cache)
 
+
 def SX_TIED_KEY(fh, cache):
     data = process_item(fh, cache)
-    key  = process_item(fh, cache)
+    key = process_item(fh, cache)
     return data
-    
+
+
 def SX_TIED_IDX(fh, cache):
     data = process_item(fh, cache)
     # idx's are always big-endian dumped by storable's freeze/nfreeze I think
-    indx_in_array = unpack('>I', fh.read(4))[0]
+    indx_in_array = SX_NETINT(fh, cache)
     return data
 
+
 def SX_HOOK(fh, cache):
-    flags = unpack('B', fh.read(1))[0]
+    flags = _read_unsigned_byte(fh)
 
     while flags & int(0x40):   # SHF_NEED_RECURSE
-        #print("SHF_NEED_RECURSE")
         dummy = process_item(fh, cache)
-        #print(dummy)
-        flags = unpack('B', fh.read(1))[0]
-        #print("flags:"+str(flags))
-
-    #print("recursive done")
+        flags = _read_unsigned_byte(fh)
 
     if flags & int(0x20):   # SHF_IDX_CLASSNAME
-        #print("SHF_IDX_CLASSNAME")
-        #print("where:"+str(fh.tell()))
         if flags & int(0x04):   # SHF_LARGE_CLASSLEN
-            #print("SHF_LARGE_CLASSLEN")
             # TODO: test
-            indx = unpack('>I', fh.read(4))[0]
+            fmt = '>I'
+            indx = unpack(fmt, fh.read(calcsize(fmt)))[0]
         else:
-            indx = unpack('B', fh.read(1))[0]
-        #print("classindx:"+str(indx))
+            indx = _read_unsigned_byte(fh)
         package_name = cache['classes'][indx]
     else:
-        #print("where:"+str(fh.tell()))
         if flags & int(0x04):   # SHF_LARGE_CLASSLEN
-            #print("SHF_LARGE_CLASSLEN")
             # TODO: test
             # FIXME: is this actually possible?
             class_size = _read_size(fh, cache)
         else:
-            class_size = unpack('B', fh.read(1))[0]
-            #print("size:"+str(class_size))
+            class_size = _read_unsigned_byte(fh)
 
         package_name = fh.read(class_size)
         cache['classes'].append(package_name)
-        #print("size:"+str(class_size)+",package:"+str(package_name))
 
     arguments = {}
 
-    str_size = 0
     if flags & int(0x08):   # SHF_LARGE_STRLEN
-        #print("SHF_LARGE_STRLEN")
         str_size = _read_size(fh, cache)
     else:
-        #print("where:"+str(fh.tell()))
-        str_size = unpack('B', fh.read(1))[0]
+        str_size = _read_unsigned_byte(fh)
 
     if str_size:
         frozen_str = fh.read(str_size)
-        #print("size:"+str(str_size)+",frozen_str:"+str(frozen_str))
         arguments[0] = frozen_str
 
-    list_size = 0
     if flags & int(0x80):   # SHF_HAS_LIST
-        #print("SHF_HAS_LIST")
         if flags & int(0x10):   # SHF_LARGE_LISTLEN
-            #print("SHF_LARGE_LISTLEN")
-            #print("where:"+str(fh.tell()))
             list_size = _read_size(fh, cache)
         else:
-            list_size = unpack('B', fh.read(1))[0]
+            list_size = _read_unsigned_byte(fh)
 
-
-    #print("list_size:"+str(list_size))
-    for i in range(0,list_size):
-        indx_in_array = unpack('>I', fh.read(4))[0]
-        #print("indx:"+str(indx_in_array))
-        if indx_in_array in cache['objects']:
-            arguments[i+1] = cache['objects'][indx_in_array]
-        else:
-            arguments[i+1] = None
+        for i in xrange(list_size):
+            fmt = '>I'
+            indx_in_array = unpack(fmt, fh.read(calcsize(fmt)))[0]
+            arguments[i + 1] = cache['objects'].get(indx_in_array)
 
     # FIXME: implement the real callback STORABLE_thaw() still, for now, just
     # return the dictionary 'arguments' as data
-    type = flags & int(0x03) # SHF_TYPE_MASK 0x03
-    #print("flags:"+str(type))
+    type = flags & int(0x03)  # SHF_TYPE_MASK 0x03
     data = arguments
     if type == 3:  # SHT_EXTRA
         # TODO
@@ -217,20 +223,17 @@ def SX_HOOK(fh, cache):
         #print("SHT_HASH")
         pass
 
-
     return data
+
 
 def SX_FLAG_HASH(fh, cache):
     # TODO: NOT YET IMPLEMENTED!!!!!!
-    #print("SX_FLAG_HASH:where:"+str(fh.tell()))
-    flags = unpack('B', fh.read(1))[0]
-    size  = _read_size(fh, cache)
-    #print("size:"+str(size))
-    #print("flags:"+str(flags))
+    flags = _read_unsigned_byte(fh)
+    size = _read_size(fh, cache)
     data = {}
-    for i in range(0,size):
+    for i in xrange(size):
         value = process_item(fh, cache)
-        flags = unpack('B', fh.read(1))[0]
+        flags = _read_unsigned_byte(fh)
         keysize = _read_size(fh, cache)
         key = None
         if keysize:
@@ -238,6 +241,17 @@ def SX_FLAG_HASH(fh, cache):
         data[key] = value
 
     return data
+
+
+def SX_VSTRING(fh, cache):
+    value = SX_SCALAR(fh, cache)
+    return tuple(int(x) for x in value[1:].split('.'))
+
+
+def SX_LVSTRING(fh, cache):
+    value = SX_LSCALAR(fh, cache)
+    return tuple(int(x) for x in value[1:].split('.'))
+
 
 # *AFTER* all the subroutines
 engine = {
@@ -247,7 +261,7 @@ engine = {
     '\x03': SX_HASH,        # ( 3): Hash forthcoming (size, key/value pair list)
     '\x04': SX_REF,         # ( 4): Reference to object forthcoming
     '\x05': SX_UNDEF,       # ( 5): Undefined scalar
-    '\x06': SX_INTEGER,     # ( 6): Undefined scalar
+    '\x06': SX_INTEGER,     # ( 6): Integer forthcoming
     '\x07': SX_DOUBLE,      # ( 7): Double forthcoming
     '\x08': SX_BYTE,        # ( 8): (signed) byte forthcoming
     '\x09': SX_NETINT,      # ( 9): Integer in network order forthcoming
@@ -256,6 +270,8 @@ engine = {
     '\x0c': SX_TIED_HASH,   # (12): Tied hash forthcoming
     '\x0d': SX_TIED_SCALAR, # (13): Tied scalar forthcoming
     '\x0e': SX_SV_UNDEF,    # (14): Perl's immortal PL_sv_undef
+    '\x0f': SX_SV_YES,      # (15): Perl's immortal PL_sv_yes
+    '\x10': SX_SV_NO,       # (16): Perl's immortal PL_sv_no
     '\x11': SX_BLESS,       # (17): Object is blessed
     '\x12': SX_IX_BLESS,    # (18): Object is blessed, classname given by index
     '\x13': SX_HOOK,        # (19): Stored via hook, user-defined
@@ -265,89 +281,109 @@ engine = {
     '\x17': SX_UTF8STR,     # (23): UTF-8 string forthcoming (small)
     '\x18': SX_LUTF8STR,    # (24): UTF-8 string forthcoming (large)
     '\x19': SX_FLAG_HASH,   # (25): Hash with flags forthcoming (size, flags, key/flags/value triplet list)
+    '\x1d': SX_VSTRING,     # (29): vstring forthcoming (small)
+    '\x1e': SX_LVSTRING,    # (30): vstring forthcoming (large)
 }
 
-exclude_for_cache = dict({
-    '\x00':True, '\x0b':True, '\x0c':True, '\x0d':True, '\x11':True, '\x12':True
-})
+
+exclude_for_cache = {'\x00', '\x0b', '\x0c', '\x0d', '\x11', '\x12'}
+
 
 def handle_sx_object_refs(cache, data):
     iterateelements = None
     if type(data) is list:
-        iterateelements = enumerate(iter(data))
+        iterateelements = enumerate(data)
     elif type(data) is dict:
         iterateelements = data.iteritems()
     else:
         return
-    
-    for k,item in iterateelements:
+
+    for k, item in iterateelements:
         if type(item) is list or type(item) is dict:
             handle_sx_object_refs(cache, item)
-        if type(item) is tuple:
+        elif type(item) is tuple:
             data[k] = cache['objects'][item[1]]
     return data
 
+
 def process_item(fh, cache):
     magic_type = fh.read(1)
-    #print('magic:'+str(unpack('B',magic_type)[0])+",where:"+str(fh.tell())+',will do:'+str(engine[magic_type]))
-    if magic_type not in exclude_for_cache:
-        i = cache['objectnr']
-        cache['objectnr'] = cache['objectnr']+1
-        #print("set i:"+str(i))
-        cache['objects'][i] = engine[magic_type](fh, cache)
-        #print("set i:"+str(i)+",to:"+str(cache['objects'][i]))
-        return cache['objects'][i]
+    if magic_type in exclude_for_cache:
+        data = engine[magic_type](fh, cache)
     else:
-        return engine[magic_type](fh, cache)
-            
-def thaw(frozen_data):
-    fh = cStringIO.StringIO(frozen_data)
-    data = deserialize(fh);
-    fh.close();
+        i = cache['objectnr']
+        cache['objectnr'] += 1
+        data = engine[magic_type](fh, cache)
+        cache['objects'][i] = data
     return data
+
+
+def thaw(frozen_data):
+    fh = StringIO.StringIO(frozen_data)
+    return deserialize(fh)
+
 
 def retrieve(file):
-    fh = open(file, 'rb')
-    ignore = fh.read(4)
-    data = None
-    if ignore == 'pst0':
-        data = deserialize(fh)
-    fh.close()
-    return data
+    with open(file, 'rb') as fh:
+        file_magic = fh.read(4)
+        if file_magic == 'pst0':
+            return deserialize(fh)
+
+
+def _read_unsigned_byte(fh):
+    return unpack('B', fh.read(1))[0]
+
 
 def deserialize(fh):
-    magic = fh.read(1)
-    byteorder = '>'
-    if magic == '\x05':
-        version = fh.read(1)
-        #print("OK:nfreeze") 
-        #pass
-    if magic == '\x04':
-        version = fh.read(1)
-        size  = unpack('B', fh.read(1))[0]
+    magic_byte = _read_unsigned_byte(fh)
+    is_network_byte_order = (magic_byte & 1) == 1
+    major_version_number = magic_byte >> 1
+    minor_version_number = _read_unsigned_byte(fh)
+
+    nvsize = 8  # Size of double in bytes
+    integer_formats = {
+        2: 'H',
+        4: 'I',
+        8: 'Q',
+    }
+    double_formats = {
+        4: 'f',
+        8: 'd',
+    }
+    if is_network_byte_order:
+        byteorder = '!'
+        # TODO: unsure what these values should be when reading a net-order
+        # file
+        intsize = 4
+        longsize = 8
+        ptrsize = 4
+    else:
+        size = _read_unsigned_byte(fh)
         archsize = fh.read(size)
-        #print("OK:freeze:" + str(byteorder))
 
         # 32-bit ppc:     4321
         # 32-bit x86:     1234
         # 64-bit x86_64:  12345678
-        
+
         if archsize == '1234' or archsize == '12345678':
             byteorder = '<'
         else:
             byteorder = '>'
 
-        somethingtobeinvestigated = fh.read(4)
+        intsize, longsize, ptrsize = unpack('3B', fh.read(3))
+        if (major_version_number, minor_version_number) >= (2, 2):
+            nvsize = _read_unsigned_byte(fh)
+            if nvsize > 8:
+                raise ValueError('Cannot handle 16 byte doubles')
 
-    #print('version:'+str(unpack('B', version)[0]));
-    cache = { 
-        'objects'           : {},
-        'objectnr'          : 0,
-        'classes'           : [],
-        'has_sx_object'     : False,
-        'size_unpack_fmt'   : byteorder + 'I',
-        'int_unpack_fmt'    : byteorder + 'Q',
-        'double_unpack_fmt' : byteorder + 'd'
+    cache = {
+        'objects': {},
+        'objectnr': 0,
+        'classes': [],
+        'has_sx_object': False,
+        'size_unpack_fmt': byteorder + integer_formats[intsize],
+        'int_unpack_fmt': byteorder + integer_formats[longsize],
+        'double_unpack_fmt': byteorder + double_formats[nvsize],
     }
     data = process_item(fh, cache)
 
