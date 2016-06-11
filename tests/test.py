687d754ad2790c13ea24a1e76482ffc21d5a2dc8
Quasar Jarosz <jjarosz@uams.edu>
2016-06-11 13:19:24 -0500

    2to3 on tests

--- tests/test.py
+++ tests/test.py
@@ -138,7 +138,7 @@ def mythaw(infile):
     # thaw() it
     try:
         data = storable.thaw(data)
-    except Exception,e:
+    except Exception as e:
         traceback.print_exc(e)
 
     return data
@@ -301,7 +301,7 @@ class TestStorable(unittest.TestCase):
             result_we_need = outfh.read()
             #print(str(result_we_need))
             outfh.close()
-        except Exception,e:
+        except Exception as e:
             traceback.print_exc(e)
 
         # dump it
@@ -314,8 +314,8 @@ class TestStorable(unittest.TestCase):
         # check
         try:
             self.assertEqual(str(data), str(result_we_need))
-        except AssertionError, e:
-            print('infile: '+str(infile)+' ,outfile: '+str(outfile))
+        except AssertionError as e:
+            print(('infile: '+str(infile)+' ,outfile: '+str(outfile)))
             raise e
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestStorable)
