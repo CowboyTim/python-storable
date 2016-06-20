52faf0b1938fedcf7604e7875b5124f021fd58ff
Quasar Jarosz <jjarosz@uams.edu>
2016-06-20 16:59:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup
 
 setup(
     name             = 'storable',
-    version          = '0.1.0',
-    description      = 'Python Perl Storable module',
-    author           = 'CowboyTim',
-    author_email     = 'aardbeiplantje@gmail.com',
-    url              = 'http://github.com/CowboyTim/python-storable',
+    version          = '0.2.0',
+    description      = 'Python Perl Storable module, python3 port',
+    author           = 'CowboyTim <aardbeiplantje@gmail.com>, Quasar Jarosz <quasar@ja.rosz.org>',
+    author_email     = 'quasar@ja.rosz.org',
+    url              = 'https://github.com/quasarj/python-storable3',
     license          = 'LICENSE.txt',
     py_modules       = ['storable'],
-    long_description = open('README.txt').read(),
+    long_description = open('README.md').read(),
     classifiers      = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Perl',
         'Programming Language :: Python',
